24267e8eadf772693b9e407113eff19393a28fb3
Dan <danielsantosv8@usp.br>
2020-10-17 19:41:36 -0300

    Aula 11

--- blog/website/views.py
+++ blog/website/views.py
@@ -4,4 +4,11 @@ from django.shortcuts import render
 # os templates devem ficar sempre na pasta "templates" do app em questao
 
 def hello_blog(request):
-    return render(request, 'index.html')
+    # A variavel data sera o response ao template, isto eh, agora podemos usar essa variavel no arquivo html do template
+    # Isso eh possivel gracas ao framework que o django utiliza internamente chamado "ginger", no qual podemos inserir
+    # codigos basicos de python dentro do aquivo html.
+
+    # Mais intrucoes de uso no arquivo index.html
+    lista = ['Python', 'Django', 'Linux']
+    data = {'name': 'Curso de Django 3', 'lista': lista}
+    return render(request, 'index.html', data)
