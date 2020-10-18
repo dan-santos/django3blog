81a95f1da402ee1ff1d6919f4de96c7331cf9240
Dan <danielsantosv8@usp.br>
2020-10-17 23:49:28 -0300

    Aula 16

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -6,11 +6,14 @@ class PostAdmin(admin.ModelAdmin):
     # dita para o django como a lista de posts (da pagina admin) devem ser exibidos
     # para aplica-lo, basta colocarmos como segundo argumento na hora de registrar o model
     # para ver mais, https://docs.djangoproject.com/en/3.1/ref/contrib/admin/
-    list_display = ['title', 'subtitle']
+    list_display = ['title', 'subtitle', 'categories']
 
     # vai habilitar uma barra de busca na qual retornara para o usuario qualquer recorrencia da palavra buscada
     # nos campos listados na list
-    search_fields = ['title', 'subtitle']
+    search_fields = ['title', 'subtitle', 'categories']
+
+    # Se soh quisermos mostrar determinados campos na tela de detalhe do post, digitamos o seguinte comando:
+    # fields = ['title']
 
 # Register your models here.
 # Isto eh, sempre que quisermos que os models aparecam na pagina do admin, precisamos registra-los aqui, dessa forma:
--- blog/website/models.py
+++ blog/website/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # todo model precisa ser uma classe
 # a superclasse indica pro django que a classe é deve ser tranformada em uma tabela sql
 
+# enums (TextChoices)
+# pode ser feito para muitas coisas, como para reduzir espaco no banco de dados, pois, ao inves de salvar o
+# conteudo completo da variavel (nome da categoria), salva apenas sua abreviacao
+class Categories(models.TextChoices):
+    TECH = 'TC', 'Tecnologia',
+    CR = 'CR', 'Curiosidades',
+    GR = 'GR', 'Geral'
+
 # todo campo eh tratado como um ___Field, que indica que aquele atributo será uma coluna da tabela
 # CharField == varchar()
 # Para ver todos os tipos permitidos: https://docs.djangoproject.com/pt-br/3.1/ref/models/fields/
@@ -10,9 +18,15 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2, # siglas
+        choices=Categories.choices, # setando o enum
+        default=Categories.GR # setando qual dos marcadores do enum sera o padrao
+    )
 
     def __repr__(self):
         return self.title
 
     def __str__(self):
         return self.__repr__()
+
